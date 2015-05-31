48f1270e18256a77129822203530c52d0a6fd982
Stuart Osborne <stuart.osborne@transmode.se>
2015-05-31 11:50:50 +0200

    Shutdown corrected.

--- lib/fiberator.ex
+++ lib/fiberator.ex
@@ -4,24 +4,35 @@ defmodule Fiberator do
 
   def main(argv) do
     {:ok, cache_pid} = prepare_cache()
-    result = request_input() |> fib_cache(cache_pid)
-    IO.write("The fiborator says: #{result}\n")
-    get_fib_cache(cache_pid) |> write_fib_cache_to_file()
-    fib_cache_stop(cache_pid)
+    handle_input(cache_pid)
   end
 
-  defp request_input() do
-    IO.gets("Enter integer to fiberate (q to quiterate): ") |> process_input
+  defp handle_input(cache_pid) do
+    result = request_input(cache_pid) |> fib_cache(cache_pid)
+    IO.write("The fiborator says: #{result}\n")
+    handle_input(cache_pid)
+  end
+
+  defp request_input(cache_pid) do
+    IO.gets("Enter integer to fiberate (q to quiterate): ")
+    |> process_input(cache_pid)
   end
 
   defp prepare_cache() do
     read_fib_cache_from_file() |> fib_cache_start
   end
 
-  defp process_input("q\n") do
-     Process.exit(self, "quiterated!")
+  defp process_input("q\n", cache_pid) do
+    handle_shutdown(cache_pid)
+    Process.exit(self, "quiterated!")
   end
-  defp process_input(data) do
+  defp process_input(data, _) do
     String.strip(data) |> String.to_integer
   end
+
+  defp handle_shutdown(cache_pid) do
+    get_fib_cache(cache_pid) |> write_fib_cache_to_file()
+    fib_cache_stop(cache_pid)
+  end
+
 end
